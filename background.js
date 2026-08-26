6cdd8152b4a67a94ca377a83d172c35eaa58cfdb
a catgirl <paws@a-catgirl.dev>
2026-08-26 14:14:06 -0400

    Revert xcancel redirections
    This reverts commits 13c00f0c63ca9c0360cc78a8e2f8e85c0d418aa1 and
    efa2d44bcdfbddf5f60fe2e7f6f461756ed3711f.

--- background.js
+++ background.js
@@ -4,9 +4,7 @@
 
 chrome.webNavigation.onBeforeNavigate.addListener(async (details) => {
     const url = new URL(details.url);
-    if (url.hostname === 'twitter.com' || url.hostname === 'x.com' ||
-        url.hostname === 'xcancel.com')
-    {
+    if (url.hostname === 'twitter.com' || url.hostname === 'x.com') {
         const newUrl = 'https://nitter.net' + url.pathname + url.search + url.hash;
         await chrome.tabs.update(details.tabId, { url: newUrl });
     }
